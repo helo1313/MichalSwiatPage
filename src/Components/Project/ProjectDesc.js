c8014e63e7b6110eda06c0ec520431b99acf05b9
Helo <62700189+helo1313@users.noreply.github.com>
2023-09-07 00:23:57 +0200

    added description desktop page

--- src/Components/Project/ProjectDesc.js
+++ src/Components/Project/ProjectDesc.js
@@ -1,11 +1,15 @@
+import useIsMobile from "../../hooks/use-is-mobile";
+
 import styles from "./ProjectDesc.module.scss";
 
 const ProjectDesc = (props) => {
+  const isMobile = useIsMobile();
   const data = props.data;
   console.log(data);
 
   return (
     <div className={`${styles.project} ${props.isActive ? styles.active : ""}`}>
+      {!isMobile && <div className={styles.projectDescriptionFrame} />}
       <div className={styles.projectDescripton}>
         <p>Project: {data.name}</p>
         <p>Place:</p>
@@ -13,7 +17,7 @@ const ProjectDesc = (props) => {
         <p>Author:</p>
         <p>Created during:</p>
         <p>Vizualization:</p>
-      </div>{" "}
+      </div>
     </div>
   );
 };

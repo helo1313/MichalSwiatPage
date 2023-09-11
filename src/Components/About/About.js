ed54529e44aad1a8160f3e770e2c6a2dca9a40fc
Helo <62700189+helo1313@users.noreply.github.com>
2023-09-11 22:30:24 +0200

    added hover in about page for desktop

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,4 +1,13 @@
+import { useState } from "react";
+
 import useIsMobile from "../../hooks/use-is-mobile";
+
+import {
+  AboutUsText,
+  OfferText,
+  IdeaText,
+  ProcessText,
+} from "../../Data/about-page-data";
 
 import House01Map from "../../images/Dookola/Dookola_Map.svg";
 import House02Map from "../../images/Lanowa/Lanowa_Map.svg";
@@ -7,7 +16,12 @@ import House03Map from "../../images/NaLini/NaLini_Map.svg";
 import styles from "./About.module.scss";
 
 const About = (props) => {
+  const [displayText, setDisplayText] = useState(AboutUsText);
+  const [activeElementIndex, setActiveElementIndex] = useState(1);
+
   const isMobile = useIsMobile();
+
+  const changeDisplayText = (text) => setDisplayText(text);
 
   return (
     <div
@@ -15,29 +29,60 @@ const About = (props) => {
     >
       <div className={styles.contentBox}>
         <ul className={styles.aboutList}>
-          <li className={styles.activeItem}>About me</li>
-          <li>Idea</li>
-          <li>Process</li>
-          <li>Contact</li>
+          <li
+            className={activeElementIndex === 1 ? styles.activeItem : ""}
+            onMouseOver={
+              !isMobile
+                ? () => {
+                    changeDisplayText(AboutUsText);
+                    setActiveElementIndex(1);
+                  }
+                : () => {}
+            }
+          >
+            About me
+          </li>
+          <li
+            className={activeElementIndex === 2 ? styles.activeItem : ""}
+            onMouseOver={
+              !isMobile
+                ? () => {
+                    changeDisplayText(IdeaText);
+                    setActiveElementIndex(2);
+                  }
+                : () => {}
+            }
+          >
+            Idea
+          </li>
+          <li
+            className={activeElementIndex === 3 ? styles.activeItem : ""}
+            onMouseOver={
+              !isMobile
+                ? () => {
+                    changeDisplayText(ProcessText);
+                    setActiveElementIndex(3);
+                  }
+                : () => {}
+            }
+          >
+            Process
+          </li>
+          <li
+            className={activeElementIndex === 4 ? styles.activeItem : ""}
+            onMouseOver={
+              !isMobile
+                ? () => {
+                    changeDisplayText(OfferText);
+                    setActiveElementIndex(4);
+                  }
+                : () => {}
+            }
+          >
+            Offer
+          </li>
         </ul>
-        <p className={styles.aboutText}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum
-          dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-          incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-          commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-          velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-          occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-          mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur
-          adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-          magna aliqua.
-        </p>
+        <p className={styles.aboutText}>{displayText}</p>
       </div>
       {isMobile && (
         <div className={styles.mapbox}>
